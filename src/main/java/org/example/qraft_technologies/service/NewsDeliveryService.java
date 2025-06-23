a5c02ee168157cbcaaf435c0a93e242a5bb5405d
soyoonjeong2328 <soyounjeong6276@gmail.com>
2025-06-24 00:11:44 +0900

    refactor: @Scheduled 기반 큐 소비 구조로 변경

--- src/main/java/org/example/qraft_technologies/service/NewsDeliveryService.java
+++ src/main/java/org/example/qraft_technologies/service/NewsDeliveryService.java
@@ -7,6 +7,7 @@ import org.example.qraft_technologies.repository.TranslatedNewsRepository;
 import org.example.qraft_technologies.websocket.WebSocketNewsSender;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,17 +19,19 @@ public class NewsDeliveryService {
     private final WebSocketNewsSender sender;
     private static final Logger logger = LoggerFactory.getLogger(NewsDeliveryService.class);
 
-    @PostConstruct
+    @Scheduled(fixedDelay = 1000) // 1초마다 실행
     public void start() {
         new Thread(() -> {
             while (true) {
                 try {
                     String newsId = newsQueue.take();
                     logger.info("뉴스 ID 수신:{}", newsId);
+
                     TranslatedNews translatedNews = repository.findById(newsId)
                             .orElseThrow(() -> new RuntimeException("뉴스 ID 찾을 수 없음 : " + newsId));
-                    sender.broadcast(translatedNews);
                     logger.info("뉴스 DB 조회 성공 : {}", translatedNews.getTitle());
+
+                    sender.broadcast(translatedNews);
                 } catch (Exception e) {
                     logger.error("[ERROR] 뉴스 전송 중 예외 발생 : {}", e.getMessage());
                 }
