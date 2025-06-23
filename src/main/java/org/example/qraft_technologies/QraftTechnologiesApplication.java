41347d54674bafd6dece0e3c29b21b81467218e7
soyoonjeong2328 <soyounjeong6276@gmail.com>
2025-06-23 21:17:00 +0900

    Initial commit

---
+++ src/main/java/org/example/qraft_technologies/QraftTechnologiesApplication.java
@@ -0,0 +1,13 @@
+package org.example.qraft_technologies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QraftTechnologiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QraftTechnologiesApplication.class, args);
+    }
+
+}
